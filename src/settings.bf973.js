4749a36f65e603b288e5b0d40a628975f3c4e4a2
zxl6852 <zxl6852123123@gmail.com>
2020-09-07 12:20:10 +0800

    ver 1.0

---
+++ src/settings.bf973.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-desktop",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scene/gameOpen.fire",orientation:"",jsList:[],bundleVers:{internal:"f1413",main:"66222"}};
